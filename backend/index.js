4cee3d77a5a41019aadcdcf7225ed0bafda727f3
Neil Quirk <73691309+Neil-Quirk@users.noreply.github.com>
2023-11-20 21:12:38 +0200

    Initial Commit
    Setup of the Server and dependencies

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+//Create our server
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const ports = process.env.PORT || 3000;
+
+//Middleware MVC pattern
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.listen(ports, () => console.log(`Listening on port ${ports}`));
